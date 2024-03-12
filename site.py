a5027bdefdd5a15ad0ddaa54383ff11837148340
romnatall <roma.natalichev@gmail.com>
2024-03-12 17:00:37 +0300

    добавил чачресурс, надеюсь производительность вырастет

--- site.py
+++ site.py
@@ -19,7 +19,7 @@ class Ensemble:
 
 
 # Загрузка модели и предобработчика
-@st.cache_data
+@st.cache_resource
 def load_model():
     with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
